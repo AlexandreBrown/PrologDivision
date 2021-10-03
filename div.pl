ddd34853c5da4b60d7fcbf542b9fffe40c540b69
user <alexandrebrown97@gmail.com>
2021-10-03 11:30:11 -0400

    Simplified division operator

--- div.pl
+++ div.pl
@@ -1,25 +1,16 @@
+div(_, 0, nan, nan).
+
+div(0, _, 0, 0).
+
+div(A, A, 1, 0).
+
+div(A, B, 0, A) :- A < B.
+
 div(A, B, DivResult, DivRest) :-
-    B \== 0,
-    (
-        (
-            A == 0,
-            DivResult is 0,
-            DivRest is 0
-        );
-        (
-            get_sign(A, B, Sign),
-            A2 is abs(A),
-            B2 is abs(B),
-            (
-                can_divide(A2, B2),
-                rec_div(A2, B2, 0, DivResult, DivRest, Sign)
-            );
-            (
-                DivResult is 0,
-                DivRest is A
-            )
-        )
-    ).
+    get_sign(A, B, Sign),
+    A2 is abs(A),
+    B2 is abs(B),
+    rec_div(A2, B2, 0, DivResult, DivRest, Sign).
 
 get_sign(A, B, Sign) :- 
     (
@@ -37,19 +28,13 @@ get_sign(A, B, Sign) :-
     );
     Sign is -1.
 
-can_divide(Dividend, Divisor) :- Dividend >= Divisor.
+rec_div(A, B, Accum, DivResult, DivRest, Sign) :-
+    A >= B,
+    Quotient is A - B,
+    succ(Accum, NewAccum),
+    rec_div(Quotient, B, NewAccum, DivResult, DivRest, Sign).
 
 rec_div(A, B, Accum, DivResult, DivRest, Sign) :-
-    Rest is (A - B),
-    (
-        (
-            can_divide(Rest, B),
-            succ(Accum, NewAccum),
-            rec_div(Rest, B, NewAccum, DivResult, DivRest, Sign)
-        );
-        (
-            succ(Accum, NewAccum),
-            DivResult is Sign*NewAccum,
-            DivRest is Rest
-        )
-    ).
+    A < B,
+    DivResult is Sign*Accum,
+    DivRest is A.
